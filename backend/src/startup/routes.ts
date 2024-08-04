bffa27d8496c123423b231d5cef82936f8b16b90
camescasse <e.camescasse@gmail.com>
2024-08-04 19:11:22 -0400

    add and use json, cors, helmet and morgan dependencies

--- backend/src/startup/routes.ts
+++ backend/src/startup/routes.ts
@@ -1,10 +1,17 @@
-import { Express } from 'express';
+import cors from 'cors';
+import { Express, json } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import generator from '../routes/generator';
 import home from '../routes/home';
 
 function routes(app: Express) {
+  app.use(json());
+  app.use(cors());
+  app.use(helmet());
+  app.use(morgan('tiny'));
   app.use('/', home);
-  app.use('/generator', generator);
+  app.use('/api/generator', generator);
 }
 
 export default routes;
