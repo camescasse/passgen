f4b8d3dd3d7ead1abd3b3c6c13e0240961b359a2
camescasse <e.camescasse@gmail.com>
2024-08-04 19:11:35 -0400

    fix route

--- backend/src/routes/generator.ts
+++ backend/src/routes/generator.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/generator', async (req, res) => {});
+router.post('/', async (req, res) => {
+  res.send('works');
+});
 
 export default router;
