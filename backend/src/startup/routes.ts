65e0cd338b25e9719efc28feab59c973a366bc6b
camescasse <e.camescasse@gmail.com>
2024-08-04 19:01:50 -0400

    define type for routes function

--- backend/src/startup/routes.ts
+++ backend/src/startup/routes.ts
@@ -1,7 +1,8 @@
+import { Express } from 'express';
 import generator from '../routes/generator';
 import home from '../routes/home';
 
-function routes(app) {
+function routes(app: Express) {
   app.use('/', home);
   app.use('/generator', generator);
 }
