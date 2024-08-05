768e4b5659fbd83f0aebfc8ddcc4683bc1ad309b
camescasse <e.camescasse@gmail.com>
2024-08-04 22:35:48 -0400

    add types to error middleware parameters

--- backend/src/middleware/error.ts
+++ backend/src/middleware/error.ts
@@ -1,6 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
 import logger from '../services/logger';
 
-export default function (err, req, res, next) {
+export default function (err: Error, req: Request, res: Response, next: NextFunction) {
   logger.error(err.message, err);
   res.status(500).send('Server failure.');
 }
