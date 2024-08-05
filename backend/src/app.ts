3eab81762022a6a1ade74781e28bc8866b4b911a
camescasse <e.camescasse@gmail.com>
2024-08-04 22:53:15 -0400

    update config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,9 +5,11 @@ import routes from './startup/routes';
 const app = express();
 routes(app);
 
+//Local development
 if (process.env.NODE_ENV !== 'production') {
   const port = process.env.PORT || 3000;
   app.listen(port, () => logger.info(`Listening on port ${port}...`));
 }
 
+//Serverless deployment
 export default app;
