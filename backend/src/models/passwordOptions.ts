eebee1f24ebef4daf072d42ab9e8c68f17bd01e4
camescasse <e.camescasse@gmail.com>
2024-08-04 20:22:15 -0400

    add validation function

--- backend/src/models/passwordOptions.ts
+++ backend/src/models/passwordOptions.ts
@@ -1,6 +1,22 @@
-export default interface PasswordOptions {
+import Joi from 'joi';
+
+interface PasswordOptions {
   uppercase?: boolean;
   lowercase?: boolean;
   numbers?: boolean;
   symbols?: boolean;
 }
+
+function validatePasswordOptions(options: PasswordOptions) {
+  const schema = Joi.object({
+    length: Joi.number().min(4).max(40).required(),
+    uppercase: Joi.boolean(),
+    lowercase: Joi.boolean(),
+    numbers: Joi.boolean(),
+    symbols: Joi.boolean(),
+  });
+
+  return schema.validate(options);
+}
+
+export { PasswordOptions, validatePasswordOptions };
